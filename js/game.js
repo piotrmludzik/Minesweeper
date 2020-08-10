c2f78b9ebe9b7dd13d9c4ab51243acd26a6551fb
appuser <appuser@journey2-84b5d4f966-4plmw>
2020-08-10 17:49:48 +0000

    starter project

---
+++ js/game.js
@@ -0,0 +1,56 @@
+const game = {
+    init: function () {
+        this.drawBoard();
+
+        // TODO: do the rest of the game setup here (eg. add event listeners)
+
+    },
+
+    drawBoard: function () {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const rows = parseInt(urlParams.get('rows'));
+        const cols = parseInt(urlParams.get('cols'));
+        const mineCount = parseInt(urlParams.get('mines'));
+        const minePlaces = this.getRandomMineIndexes(mineCount, cols, rows);
+
+        let gameField = document.querySelector(".game-field");
+        this.setGameFieldSize(gameField, rows, cols);
+        let cellIndex = 0
+        for (let row = 0; row < rows; row++) {
+            const rowElement = this.addRow(gameField);
+            for (let col = 0; col < cols; col++) {
+                this.addCell(rowElement, row, col, minePlaces.has(cellIndex));
+                cellIndex++;
+            }
+        }
+    },
+    getRandomMineIndexes: function (mineCount, cols, rows) {
+        const cellCount = cols * rows;
+        let mines = new Set();
+        do {
+            mines.add(Math.round(Math.random() * (cellCount - 1)));
+        } while (mines.size < mineCount && mines.size < cellCount);
+        return mines;
+    },
+    setGameFieldSize: function (gameField, rows, cols) {
+        gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
+        gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
+    },
+    addRow: function (gameField) {
+        gameField.insertAdjacentHTML(
+            'beforeend',
+            '<div class="row"></div>'
+        );
+        return gameField.lastElementChild;
+    },
+    addCell: function (rowElement, row, col, isMine) {
+        rowElement.insertAdjacentHTML(
+            'beforeend',
+            `<div class="field${isMine ? ' mine' : ''}" 
+                        data-row="${row}" 
+                        data-col="${col}"></div>`);
+    },
+};
+
+game.init();
