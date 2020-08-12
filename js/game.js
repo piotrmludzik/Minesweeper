5638c96b8230286d69287f791aec6a3e4573bd56
Piotr Młudzik <piotr.mludzik@gmail.com>
2020-08-12 22:19:45 +0200

    field opening feature() working

--- js/game.js
+++ js/game.js
@@ -1,56 +1,127 @@
-const game = {
-    init: function () {
-        this.drawBoard();
 
-        // TODO: do the rest of the game setup here (eg. add event listeners)
-
-    },
-
-    drawBoard: function () {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const rows = parseInt(urlParams.get('rows'));
-        const cols = parseInt(urlParams.get('cols'));
-        const mineCount = parseInt(urlParams.get('mines'));
-        const minePlaces = this.getRandomMineIndexes(mineCount, cols, rows);
-
-        let gameField = document.querySelector(".game-field");
-        this.setGameFieldSize(gameField, rows, cols);
-        let cellIndex = 0
-        for (let row = 0; row < rows; row++) {
-            const rowElement = this.addRow(gameField);
-            for (let col = 0; col < cols; col++) {
-                this.addCell(rowElement, row, col, minePlaces.has(cellIndex));
-                cellIndex++;
-            }
-        }
-    },
-    getRandomMineIndexes: function (mineCount, cols, rows) {
-        const cellCount = cols * rows;
-        let mines = new Set();
-        do {
-            mines.add(Math.round(Math.random() * (cellCount - 1)));
-        } while (mines.size < mineCount && mines.size < cellCount);
-        return mines;
-    },
-    setGameFieldSize: function (gameField, rows, cols) {
-        gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
-        gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
-    },
-    addRow: function (gameField) {
-        gameField.insertAdjacentHTML(
-            'beforeend',
-            '<div class="row"></div>'
-        );
-        return gameField.lastElementChild;
-    },
-    addCell: function (rowElement, row, col, isMine) {
-        rowElement.insertAdjacentHTML(
-            'beforeend',
-            `<div class="field${isMine ? ' mine' : ''}" 
-                        data-row="${row}" 
-                        data-col="${col}"></div>`);
-    },
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const rows = parseInt(urlParams.get('rows'));
+const cols = parseInt(urlParams.get('cols'));
+const mineCount = parseInt(urlParams.get('mines'));
+const fieldType = {
+    empty: 'field',
+    mine: 'field mine',
+    flag: 'field flag'
 };
 
+
+const game = {
+    init: function () {
+        function drawBoard () {
+            const minePlaces = getRandomMineIndexes(mineCount, cols, rows);
+    
+            let gameField = document.querySelector(".game-field");
+            setGameFieldSize(gameField, rows, cols);
+            let cellIndex = 0
+            for (let row = 0; row < rows; row++) {
+                const rowElement = addRow(gameField);
+                for (let col = 0; col < cols; col++) {
+                    addCell(rowElement, row, col, minePlaces.has(cellIndex));
+                    cellIndex++;
+                }
+            }
+        }
+
+        function getRandomMineIndexes (mineCount, cols, rows) {
+            const cellCount = cols * rows;
+            let mines = new Set();
+            do {
+                mines.add(Math.round(Math.random() * (cellCount - 1)));
+            } while (mines.size < mineCount && mines.size < cellCount);
+            return mines;
+        }
+    
+        function setGameFieldSize (gameField, rows, cols) {
+            gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
+            gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
+        }
+    
+        function addRow (gameField) {
+            gameField.insertAdjacentHTML(
+                'beforeend',
+                '<div class="row"></div>'
+            );
+            return gameField.lastElementChild;
+        }
+    
+        function addCell (rowElement, row, col, isMine) {
+            rowElement.insertAdjacentHTML(
+                'beforeend',
+                `<div class="field${isMine ? ' mine' : ''}" 
+                            data-row="${row}" 
+                            data-col="${col}"></div>`);
+        }
+
+        drawBoard();
+    },
+
+    engine: function () {
+        function clickHandlerOnField(event) {
+            const cField = event.target;
+            const cFieldPos = {
+                x: parseInt(cField.dataset.col),
+                y: parseInt(cField.dataset.row)
+            };
+
+            function checkNeighborMineNumber() {
+                const board = Array.from(document.querySelectorAll('div[data-row]'));  // all fields on the board
+
+                function getFieldToCheck(seekedX, seekedY) {
+                    let field = board.find(function(node) {
+                        return parseInt(node.dataset.col) === seekedX && parseInt(node.dataset.row) === seekedY;
+                    });
+                    return field
+                }
+
+                // ------------- checkNeighborMineNumber() main code -------------
+                let mineNumber = 0;
+                for (x = -1; x <= 1; x++) {
+                    for (y = -1; y <= 1; y++) {
+                        posX = cFieldPos.x + x;
+                        posY = cFieldPos.y + y;
+
+                        // boundary conditions
+                        if (x == 0 && y === 0) { continue; }  // the own field
+                        if (posX < 0 || posX >= cols  || posY < 0 || posY >= rows) {continue; }  // out of the board
+
+                        // looks for the mine
+                        let fieldToCheck = getFieldToCheck(posX, posY);
+                        if (fieldToCheck.className === fieldType.mine) {
+                            mineNumber++;
+                        }
+                    }
+                }
+
+                return mineNumber;
+            }
+
+            // ------------- clickHandlerOnField() main code -------------
+            console.log(`clicked coordinates: ${cFieldPos.x}x${cFieldPos.y}; field type: ${cField.className}`);  // Note: the development code.
+
+            // The game main logic
+            switch (cField.className) {  // the field type
+                case fieldType.empty:
+                    let mineNumber = checkNeighborMineNumber();
+                    cField.setAttribute('class', 'open-field');
+                    if (mineNumber > 0) { cField.textContent = mineNumber; }
+                    break;
+                case fieldType.mine:
+                    cField.setAttribute('class', 'show-mines');
+                    break;
+            }
+        }
+
+        const boardContainer = document.querySelector('.game-field');
+        boardContainer.addEventListener('click', clickHandlerOnField);
+    }
+};
+
+
 game.init();
+game.engine();
